ed35cc9e7c9f45aec8b37a8cf25fb30f37197bad
zhangchao <zf1721418@buaa.edu.cn>
2020-11-15 22:57:16 +0800

    Update StockTradingEnv class

--- env/CustomEnv.py
+++ env/CustomEnv.py
@@ -1,1 +1,68 @@
-class StockTradingEnv
+import random
+import gym
+from gym import spaces
+import numpy as np
+
+# 最大账户余额
+MAX_ACCOUNT_BALANCE = 2147483647
+# 最大持股数
+MAX_NUM_SHARES = 2147483647
+# 最高股价
+MAX_SHARE_PRICE = 5000
+# 最大交易量
+MAX_VOLUME = 1000e8
+MAX_AMOUNT = 3e10
+MAX_OPEN_POSITIONS = 5
+MAX_STEPS = 20000
+MAX_DAY_CHANGE = 1
+
+INITIAL_ACCOUNT_BALANCE = 10000
+
+
+class StockTradingEnv(gym.Env):
+    """基于gym构建简易的股票交易环境"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, df):
+        super(StockTradingEnv, self).__init__()
+
+        self.df = df
+        self.reward_range = (0, MAX_ACCOUNT_BALANCE)
+        # 动作空间格式为买入x%，卖出x%以及持有
+        self.action_space = spaces.Box(low=[0, 0], high=[3, 1], dtype=np.float16)
+        # 观测到的为最近五个价格的OHCL值
+        self.observation_space = spaces.Box(
+            low=0, high=1, shape=[19, ], dtype=np.float16)
+
+    def _next_observation(self):
+        obs = np.array([
+            self.df.loc[self.current_step, 'open'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'high'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'low'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'close'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'volume'] / MAX_VOLUME,
+            self.df.loc[self.current_step, 'amount'] / MAX_AMOUNT,
+            self.df.loc[self.current_step, 'adjustflag'] / 10,
+            self.df.loc[self.current_step, 'tradestatus'] / 1,
+            self.df.loc[self.current_step, 'pctChg'] / 100,
+            self.df.loc[self.current_step, 'peTTM'] / 1e4,
+            self.df.loc[self.current_step, 'pbMRQ'] / 100,
+            self.df.loc[self.current_step, 'psTTM'] / 100,
+            self.df.loc[self.current_step, 'pctChg'] / 1e3,
+
+        ])
+
+        return obs
+
+    def _take_action(self):
+
+
+    def step(self, action):
+
+
+
+    def reset(self):
+
+    def rencer(self):
+
+
