5af42d221b98e0edc35413bf528db6e3470b2237
zhangchao <zf1721418@buaa.edu.cn>
2020-11-16 20:00:33 +0800

    Update StockTradingEnv class

--- env/CustomEnv.py
+++ env/CustomEnv.py
@@ -35,6 +35,7 @@ class StockTradingEnv(gym.Env):
             low=0, high=1, shape=[19, ], dtype=np.float16)
 
     def _next_observation(self):
+        # 注意归一化处理
         obs = np.array([
             self.df.loc[self.current_step, 'open'] / MAX_SHARE_PRICE,
             self.df.loc[self.current_step, 'high'] / MAX_SHARE_PRICE,
@@ -52,6 +53,7 @@ class StockTradingEnv(gym.Env):
             self.balance / MAX_ACCOUNT_BALANCE,
             self.max_net_worth / MAX_ACCOUNT_BALANCE,
             self.shares_held / MAX_NUM_SHARES,
+            # 买入成本
             self.cost_basis / MAX_SHARE_PRICE,
             self.total_shares_sold / MAX_NUM_SHARES,
             self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE)
@@ -59,11 +61,48 @@ class StockTradingEnv(gym.Env):
 
         return obs
 
-    def _take_action(self):
+    def _take_action(self, action):
+        # 将当前股价设置为时间步下的随机价格
+        current_price = random.uniform(
+            self.df.loc[self.current_step, 'open'], self.df.loc[self.current_step, 'close'])
 
+        action_type = action[0]
+        action_amount = action[1]
+
+        if action_type < 1:
+            # 用账户余额的x%买入股票
+            total_possible = int(self.balance / current_price)
+            shares_bought = int(total_possible * action_amount)
+            prev_cost = self.cost_basis * self.shares_held
+            additional_cost = shares_bought * current_price
+
+            self.balance -= additional_cost
+            self.cost_basis = (
+                prev_cost + additional_cost) / (self.shares_held + shares_bought)
+            self.shares_held += shares_bought
+
+        if action_type < 2:
+            # 卖出x%的持有股票
+            shares_sold = int(self.shares_held * action_amount)
+            self.balance += shares_sold * current_price
+            self.shares_held -= shares_sold
+            self.total_shares_sold += shares_sold
+            self.total_sales_value += shares_sold * current_price
+
+        self.net_worth = self.balance + self.shares_held * current_price
+        if self.net_worth > self.max_net_worth:
+            self.max_net_worth = self.net_worth
+
+        if self.shares_held == 0:
+            self.cost_basis = 0
 
     def step(self, action):
+        self._take_action(action)
+        done = False
 
+
+
+        return obs, reward, done, {}
 
 
     def reset(self):
