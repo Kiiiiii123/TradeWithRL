1ec6b87691226104abba7878a146e901ea2f3a18
zhangchao <zf1721418@buaa.edu.cn>
2020-11-16 23:28:54 +0800

    Initialize main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+# 实现python对象的持久化存储
+import pickle
+import pandas as pd
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import dummy_vec_env
+from stable_baselines import ppo2
+from env.CustomEnv import StockTradingEnv
+
+
+
+
+def tradestock()
