144890db0016a5f746d899f7e5b71e56162d97a5
zhangchao <zf1721418@buaa.edu.cn>
2020-11-17 19:01:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 import pickle
 import pandas as pd
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import dummy_vec_env
-from stable_baselines import ppo2
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
 from env.CustomEnv import StockTradingEnv
 
 import numpy as np
@@ -15,7 +15,19 @@ font = fm.FontProperties(fname='font/wqy-microhei.ttc')
 # plt.rc('font', family='Source Han Sans CN')
 plt.rcParams['axes.unicode_minus'] = False
 
-def tradestock()
+def train_stock_trading(stock_file):
+    day_profits = []
+    df_train = pd.read_csv(stock_file)
+    df_train = df_train.sort_values('date')
 
+    env =DummyVecEnv([lambda: StockTradingEnv(df_train)])
+    model = PPO2(MlpPolicy, env, verbose=0, tensorboard_log='./log')
+    model.learn(total_timesteps=int(1e4))
+
+    df_test =
+
+
+
+def test_stock_trading()
 
 if __name__ == '__main__':
